666fca15127f501561a12df4dd570b9a28c7fc9e
yakuhito <y@kuhi.to>
2022-01-14 09:07:30 +0200

    1.0.4

--- docker-start.sh
+++ docker-start.sh
@@ -2,10 +2,7 @@
 
 chia start node
 
-trap "chia stop all -d; exit 0" SIGINT SIGTERM
-
 touch "$CHIA_ROOT/log/debug.log"
-tail -F "$CHIA_ROOT/log/debug.log" &
 
 cd /leaflet
 REPORT_METRICS=1 npm start
